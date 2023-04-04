d339778d884b9996543d7edb7a3b9abf1a627537
Fabphokie <mmaphokengsenne@gmail.com>
2023-04-04 13:03:07 +0200

    IWA

---
+++ IWA2/script.js
@@ -0,0 +1,88 @@
+
+// This is the firstname of the user 
+
+const user = 'John'
+
+
+// This is the lastname of the user 
+
+const surname = 'Smith'
+
+
+console.log(user, surname);
+
+
+
+/** (c) ACME Inc. 2010 */
+
+/*
+ * This is the date that a user created their account 
+ */
+const date = '10/07/2014'
+
+console.log(date);
+
+
+/** 
+It is important to show the following message in order to:
+*to assure users
+*to scare hackers
+*to impress investors
+*/
+
+console.warn("Security scan starting");
+
+/* It is important to let user know when they can close the page  */
+
+console.info("Please wait for scan to complete before closing the browser.");
+ 
+
+
+
+
+
+
+
+const  MAX_NUMBER = 15
+const MIN_NUMBER = -5
+const STEP_AMOUNT = 5
+
+
+const number = document.querySelector('[data-key="number"]')
+const subtract = document.querySelector('[data-key="subtract"]')
+const add = document.querySelector('[data-key="add"]')
+
+
+
+const subtractHandler = () => {
+
+    const newValue = parseInt(number.value) - STEP_AMOUNT
+    number.value = newValue
+
+    if (add.disabled === true) {
+        add.disabled =false
+    }
+
+    if (newValue <= MIN_NUMBER) {
+        subtract.disabled =true
+    }
+}
+
+const addHandler = () => {
+    const newValue = parseInt(number.value) + STEP_AMOUNT
+    number.value = newValue
+
+    if (subtract.disabled === true) {
+        subtract.disabled = false
+    }
+    if (newValue.disabled >= MAX_NUMBER) {
+        add.disabled = true
+        
+    }
+
+}
+
+
+subtract.addEventListener('click', subtractHandler)
+add.addEventListener('click', addHandler)
+
---
+++ IWA5/script.js
@@ -0,0 +1,61 @@
+const FREE_WARNING = 'Free shipping only applies to single customer orders';
+const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence'; // Add letter G on 'Warning'
+const NONE_SELECTED = 0;   //   Removed quotes
+
+
+let shipping = null;    // changed from cosntant to variable to allow reassignment
+let currency = '$';    // added quotes
+let location = 'NK';
+let customers = 1;   // removed the quotes because it cant work when its a string
+
+
+
+
+// added 'Const'
+
+const shoes = 300 * 1;
+const toys = 100 * 5;
+const shirts = 150 * NONE_SELECTED;   // removed the quotes
+const batteries = 35 * 2;                      // added '='
+const pens = 5 * NONE_SELECTED;  // removed the quotes
+
+
+// fixed the if statements and added missing brackets
+if (location === 'RSA') { 
+    shipping = 400;
+    currency = 'R'; 
+} 
+else if (location === 'NAM') {
+    shipping = 600;
+} else {
+    shipping = 800;
+
+} 
+
+// fixed the if statements and added missing brackets
+if (shoes + batteries + pens + shirts > 1000) {
+	if (location === 'NAM' && customers < 2) {
+        shipping = 0 || calcShipping();
+
+    } else if (location === 'RSA') {
+		    shipping = 0 || calcShipping();
+	}
+}
+
+
+if ( shipping === 0 && customers !== 1) {       //removed extra brackets
+    console.log(FREE_WARNING);    // Correct the varible from warning to free_warning
+}
+
+
+// corrected the if statement
+
+if (location ==='NK') {
+ console.log(BANNED_WARNING);
+ } else {
+    console.log('Price', currency, shoes + batteries + toys +pens + shirts + shipping);   // add toys to balance
+ }
+
+
+
+ 
