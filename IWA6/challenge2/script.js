678660342e95099ccfb2ee4e693a9ffcd74de98a
Fabphokie <mmaphokengsenne@gmail.com>
2023-04-05 08:29:16 +0200

    Updates

--- IWA6/challenge2/script.js
+++ IWA6/challenge2/script.js
@@ -1,5 +1,5 @@
 const rent = 400;
-const tax = '12%';
+const tax = 12;
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
@@ -8,9 +8,8 @@ const minuteOfDay = 00;
 
 // Only change below this line
 
-let balance;
 if (hourOfDay == 00 && minuteOfDay == 00) {                       
-	const taxAsDecimal = parseInt(tax) / 100;
+	const taxAsDecimal = tax / 100;
     const startingAfterTax = salary * (1 - taxAsDecimal);
     const balance = startingAfterTax - transport - food - rent;
     
